f9915f4cda9c7f9fff58d83d18f980afe043b2d8
X HUNTΞR <xhunter0228@gmail.com>
2022-07-19 23:52:51 -0500

    Update MUSEENFTMarket.sol

--- MUSEENFTMarket.sol
+++ MUSEENFTMarket.sol
@@ -69,7 +69,19 @@ contract MUSEENFTMarket is
     NFTMarketPrivateSale(marketProxyAddress) // solhint-disable-next-line no-empty-blocks
     NFTMarketFees(museeNftContract)
   {}
-
+  
+  /**
+   * @inheritdoc NFTMarketCore
+   * @dev This is a no-op function required to avoid compile errors.
+   */
+  function _transferFromEscrow(
+    address nftContract,
+    uint256 tokenId,
+    address recipient,
+    address authorizeSeller
+  ) internal override(NFTMarketCore, NFTMarketReserveAuction, NFTMarketBuyPrice) {
+    super._transferFromEscrow(nftContract, tokenId, recipient, authorizeSeller);
+  }
   /**
    * @notice Called once to configure the contract after the initial proxy deployment.
    * @dev This farms the initialize call out to inherited contracts as needed to initialize mutable variables.
